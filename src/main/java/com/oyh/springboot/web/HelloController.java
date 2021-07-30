a42c948681121bd7c936bab0bf03e7bcd3ff9811
OOHYEE <azss0470@gmail.com>
2021-07-31 01:08:41 +0900

    [feat] ResponseDto 사용

--- src/main/java/com/oyh/springboot/web/HelloController.java
+++ src/main/java/com/oyh/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.oyh.springboot.web; // 컨트롤러와 관련된 클래스들이 담긴 패키지
 
+import com.oyh.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
@@ -9,8 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 */
 @RestController
 public class HelloController {
-    @GetMapping("/hello") // HTTP Method 인 Get 의 요청을 받을 수 있는 API 를 만들어 준다.
-    public String hello() {
-        return "hello";
+    @GetMapping("/hello/dto") // HTTP Method 인 Get 의 요청을 받을 수 있는 API 를 만들어 준다.
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        /*
+        @RequestParam
+        외부에서 API 로 넘긴 파라미터를 가져오는 어노테이션
+        여기서는 외부에서 name 이란 이름으로 넘긴 파라미터를 메서드 파라미터 nam 에 저장하게된다. (왼쪽에서 넘긴 파라미터를 오른쪽에 저장)
+         */
+        return new HelloResponseDto(name, amount);
     }
 }
