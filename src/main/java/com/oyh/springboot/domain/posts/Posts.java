d1a60c179845f6265dc097c55744d3a15386bcd3
OOHYEE <azss0470@gmail.com>
2021-08-07 23:52:43 +0900

    [feat] Posts 클래스 생성

---
+++ src/main/java/com/oyh/springboot/domain/posts/Posts.java
@@ -0,0 +1,31 @@
+package com.oyh.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity // 테이블과 링크될 클래스임을 알림 - 디폴트 값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍으로 테이블 이름을 매칭한다.
+public class Posts {
+    @Id // 해당 테이블의 PK 필드를 나타낸다.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // PK의 생성규칙을 나타낸다.
+    private Long id;
+
+    @Column(length = 500, nullable = false) // 테이블의 칼럼을 나타내며 굳이 선언하지 않더라도 해당 클래스의 필드는 모두 칼럼
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
