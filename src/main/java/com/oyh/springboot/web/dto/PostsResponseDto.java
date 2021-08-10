e764338bd6851ac3fee4539d5ad32ba05d70b176
OOHYEE <azss0470@gmail.com>
2021-08-10 18:37:34 +0900

    [feat] 조회 Dto 작성

---
+++ src/main/java/com/oyh/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package com.oyh.springboot.web.dto;
+
+import com.oyh.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
