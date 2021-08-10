6f74cfae3ccaf7efbbf73bddd6092596be167fc5
OOHYEE <azss0470@gmail.com>
2021-08-10 18:54:34 +0900

    [test] 등록일/수정일 업데이트 테스트

--- src/test/java/com/oyh/springboot/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/oyh/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -38,4 +39,19 @@ public class PostsRepositoryTest {
         assertThat(posts.getContent()).isEqualTo(content);
     }
 
+    @Test
+    public void BaseTimeEntity_등록() {
+        LocalDateTime now = LocalDateTime.of(2019,6,4,0,0,0);
+        postsRepository.save(Posts.builder()
+        .title("title").content("content").author("author").build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+
+        System.out.println(" >>>> createDate=" + posts.getCreatedDate()+", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
 }
