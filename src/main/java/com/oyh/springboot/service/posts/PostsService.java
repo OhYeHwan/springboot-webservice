f8d4d1db6674d8bb74044489ae1ae5f963274999
OOHYEE <azss0470@gmail.com>
2021-08-10 17:02:33 +0900

    [feat] PostsService 생성

---
+++ src/main/java/com/oyh/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.oyh.springboot.service.posts;
+
+import com.oyh.springboot.domain.posts.PostsRepository;
+import com.oyh.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
