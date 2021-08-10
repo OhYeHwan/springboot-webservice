d12c48aa18a124f4197f57582bb4d4122f5be97b
OOHYEE <azss0470@gmail.com>
2021-08-10 18:40:14 +0900

    [comment] 어노테이션 주석 작성

--- src/main/java/com/oyh/springboot/domain/posts/Posts.java
+++ src/main/java/com/oyh/springboot/domain/posts/Posts.java
@@ -6,8 +6,8 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-@Getter
-@NoArgsConstructor
+@Getter // 롬복 어노테이션
+@NoArgsConstructor // 롬복 어노테이션
 @Entity // 테이블과 링크될 클래스임을 알림 - 디폴트 값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍으로 테이블 이름을 매칭한다.
 public class Posts {
     @Id // 해당 테이블의 PK 필드를 나타낸다.
@@ -27,5 +27,10 @@ public class Posts {
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
     }
 }
