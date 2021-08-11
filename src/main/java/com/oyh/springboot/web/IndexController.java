5c0fa73e7352e9778a611efcc96226e701e6b081
OOHYEE <azss0470@gmail.com>
2021-08-11 19:19:36 +0900

    [feat] index 머스테치 URL 맵핑

--- src/main/java/com/oyh/springboot/web/IndexController.java
+++ src/main/java/com/oyh/springboot/web/IndexController.java
@@ -1,2 +1,13 @@
-package com.oyh.springboot.web;public class IndexController {
+package com.oyh.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
 }
