6db5c9c375472a578623b8b18e20ba04d437eeb8
OOHYEE <azss0470@gmail.com>
2021-08-12 18:36:42 +0900

    [feat] PostsListResponseDto 클래스 작성

---
+++ src/main/java/com/oyh/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.oyh.springboot.web.dto;
+
+import com.oyh.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
