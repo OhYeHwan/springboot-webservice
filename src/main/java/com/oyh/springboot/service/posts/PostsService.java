111b8327b57cd21ec6050803abd7fd8fd1e4119a
OOHYEE <azss0470@gmail.com>
2021-08-12 18:33:56 +0900

    [feat] Posts 조회 로직 작성

--- src/main/java/com/oyh/springboot/service/posts/PostsService.java
+++ src/main/java/com/oyh/springboot/service/posts/PostsService.java
@@ -9,6 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class PostsService {
@@ -24,6 +27,11 @@ public class PostsService {
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 
     public PostsResponseDto findById(Long id) {
