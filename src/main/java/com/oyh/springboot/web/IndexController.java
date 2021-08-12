383502fd1afdcab03839c85fe8297dd06c0c029a
OOHYEE <azss0470@gmail.com>
2021-08-12 18:41:24 +0900

    [feat] Model 적용

--- src/main/java/com/oyh/springboot/web/IndexController.java
+++ src/main/java/com/oyh/springboot/web/IndexController.java
@@ -1,12 +1,20 @@
 package com.oyh.springboot.web;
 
+import com.oyh.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
